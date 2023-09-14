49bb770a510ac57b1da09926e3341d9906628a71
Arslanbek Astemirov <s-o-f-t@mail.ru>
2023-09-14 16:43:16 +0300

    First lesson

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {splitIntoWords} from "./01-hello-tests/01";
+
+const sentence = "Hello my friends!";
+const result = splitIntoWords(sentence);
+console.log(result);
+
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
