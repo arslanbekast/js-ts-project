91b9766dc288fe46ffe3eecc76b49c97a0b6e928
Arslanbek Astemirov <s-o-f-t@mail.ru>
2023-09-23 00:22:05 +0300

    Lesson about map method

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,13 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  const names = ["Dimych", "Sveta", "Katya", "Viktor", "Ignat"]
+  const users = [{name: "Dimych"}, {name: "Sveta"}, {name: "Katya"}, {name: "Viktor"}, {name: "Ignat"}]
+  const liElements = users.map((u, index) => <li key={index}>u.name</li>)
+
   return (
     <div className="App">
-
+      {liElements}
     </div>
   );
 }
